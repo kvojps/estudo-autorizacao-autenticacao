36c259b7775faa351a19591d0c4be921b3bc8c74
kvojps <joseferreira.santosjunior@upe.br>
2022-05-10 19:39:30 -0300

    adicionando implementação do spring security

--- src/main/java/br/upe/lapes/sisrepapi/Application.java
+++ src/main/java/br/upe/lapes/sisrepapi/Application.java
@@ -1,13 +1,47 @@
 package br.upe.lapes.sisrepapi;
 
+import java.util.ArrayList;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import br.upe.lapes.sisrepapi.modelo.TipoUsuario;
+import br.upe.lapes.sisrepapi.modelo.Usuario;
+import br.upe.lapes.sisrepapi.servico.ServicoUsuario;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	CommandLineRunner run(ServicoUsuario servicoUsuario) {
+		return args -> {
+			servicoUsuario.salvarTipoUsuario(new TipoUsuario(null, "ROLE_USER"));
+			servicoUsuario.salvarTipoUsuario(new TipoUsuario(null, "ROLE_MANAGER"));
+			servicoUsuario.salvarTipoUsuario(new TipoUsuario(null, "ROLE_ADMIN"));
+			servicoUsuario.salvarTipoUsuario(new TipoUsuario(null, "ROLE_SUPER_ADMIN"));
+
+			servicoUsuario.salvarUsuario(new Usuario(null, "Spock de vulcano", "Spock", "1234", new ArrayList()));
+			servicoUsuario.salvarUsuario(new Usuario(null, "James T. Kirk", "Kirk", "1234", new ArrayList()));
+			servicoUsuario.salvarUsuario(new Usuario(null, "Uhura", "Uhura", "1234", new ArrayList()));
+			servicoUsuario.salvarUsuario(new Usuario(null, "Magro", "magro", "1234", new ArrayList()));
+			
+			servicoUsuario.atribuirTipoAoUsuario("Kirk", "ROLE_SUPER_ADMIN");
+			servicoUsuario.atribuirTipoAoUsuario("Spock", "ROLE_USER");
+			servicoUsuario.atribuirTipoAoUsuario("Spock", "ROLE_MANAGER");
+		};
 	}
 
 }
--- src/main/java/br/upe/lapes/sisrepapi/servico/ServicoUsuario.java
+++ src/main/java/br/upe/lapes/sisrepapi/servico/ServicoUsuario.java
@@ -1,9 +1,15 @@
 package br.upe.lapes.sisrepapi.servico;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import javax.transaction.Transactional;
 
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import br.upe.lapes.sisrepapi.modelo.TipoUsuario;
@@ -17,9 +23,27 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @Transactional
 @Slf4j
-public class ServicoUsuario implements IServicoUsuario {
+public class ServicoUsuario implements IServicoUsuario, UserDetailsService {
 	private final IRepositorioUsuario repositorioUsuario;
 	private final IRepositorioTipoUsuario repositorioTipoUsuario;
+
+	@Override
+	public UserDetails loadUserByUsername(String nickname) throws UsernameNotFoundException {
+		Usuario usuario = repositorioUsuario.findByNickname(nickname);
+		if (usuario == null) {
+			log.error("Usuário não encontrado no banco de dados");
+			throw new UsernameNotFoundException("Usuário não encontrado no banco de dados");
+		} else {
+			log.info("Usuário {} encontrado no banco de dados", nickname);
+		}
+
+		Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+		usuario.getTipos().forEach(tipo -> {
+			authorities.add(new SimpleGrantedAuthority(tipo.getNome()));
+		});
+		return new org.springframework.security.core.userdetails.User(usuario.getNickname(), usuario.getSenha(),
+				authorities);
+	}
 
 	@Override
 	public Usuario salvarUsuario(Usuario usuario) {
